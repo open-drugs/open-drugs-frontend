cc939671b6c7ca1222e380fe5d2691c932547098
Constantine <kon.rafikov@yandex.ru>
2021-11-22 01:11:15 +0300

    Separate component for search field

---
+++ src/app/components/search-field/search-field.component.ts
@@ -0,0 +1,26 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-field',
+  templateUrl: './search-field.component.html',
+  styleUrls: ['./search-field.component.scss']
+})
+export class SearchFieldComponent {
+  public searchForm: FormGroup;
+  public searchQuery : string ='';
+  @Output() query: EventEmitter<string> = new EventEmitter;
+
+  constructor(
+  ) {
+    this.searchForm = new FormGroup({
+      searchField: new FormControl(''),
+    });
+  }
+
+  public emitSearchQuery ($event: string): void {
+    if ($event.length !== 0) {
+      this.query.emit($event);
+    }
+  }
+}
