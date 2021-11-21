1236b893811a340020ad0c8b4848cdf378dc5241
Constantine <kon.rafikov@yandex.ru>
2021-11-21 14:03:07 +0300

    WIP: custom button component

---
+++ src/app/components/show-more-button/show-more-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-more-button',
+  templateUrl: './show-more-button.component.html',
+  styleUrls: ['./show-more-button.component.scss']
+})
+export class ShowMoreButtonComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
