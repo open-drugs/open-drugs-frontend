87d277bea69545a3c34b9a3ab9e71403648d49ef
Said <76432281+said4505@users.noreply.github.com>
2021-11-25 16:01:24 +0500

    LISC-23. WindowWidthService (#3)
    * LISC-23.Перенести WindowWidthService
    
    * LISC-23.little fix
    
    Co-authored-by: Said <skamolov@navicons.ru>

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { WindowWidth } from '../../core/utils/window-width';
+import { WindowWidthService } from '../../core/services/browser-view/window-width.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent extends WindowWidth implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    protected windowWidthService: WindowWidthService,
+    private cdRef: ChangeDetectorRef
+  ) {
+    super(windowWidthService);
   }
 
+  ngOnInit(): void {
+    this.initWindowWidth(() => {
+      this.cdRef.markForCheck();
+    });
+
+    this.detectWindowWidth(() => {
+      this.cdRef.markForCheck();
+    });
+  }
 }
