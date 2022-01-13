e5b774152e76485b45dcac91baf67e6bff363e80
Said <skamolov@navicons.ru>
2022-01-13 14:46:20 +0500

    LISC-41.Исправить адаптивность у графика

--- src/app/pages/species/species-page.component.ts
+++ src/app/pages/species/species-page.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PlotDataService } from '../../core/services/plot-data.service';
+import { WindowWidth } from '../../core/utils/window-width';
+import { WindowWidthService } from '../../core/services/browser-view/window-width.service';
 
 @Component({
   selector: 'app-species-page',
   templateUrl: './species-page.component.html',
   styleUrls: ['./species-page.component.scss'],
 })
-export class SpeciesPageComponent {
+export class SpeciesPageComponent extends WindowWidth implements OnInit {
   public data: any[];
   public layout = {
     autosize: true,
+    legend: {},
     title: '',
     xaxis: {
       rangemode: 'tozero',
@@ -24,9 +27,26 @@ export class SpeciesPageComponent {
   };
 
   constructor(
+    public windowWidthService: WindowWidthService,
     private plotDataService: PlotDataService,
   ) {
+    super(windowWidthService);
+  }
+
+  ngOnInit(): void {
     this.setPlotData();
+
+    this.initWindowWidth(() => {
+      this.layout.legend = {
+        orientation: this.isMobile ? 'h' : '',
+      };
+    });
+
+    this.detectWindowWidth(() => {
+      this.layout.legend = {
+        orientation: this.isMobile ? 'h' : '',
+      };
+    });
   }
 
   setPlotData(): void {
