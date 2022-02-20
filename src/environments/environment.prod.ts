c07e96a7d9d0a0cad12e180a7fc919c767217d83
Constantine <kon.rafikov@yandex.ru>
2022-02-20 17:27:56 -0300

    Added species list mock

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   apiUrl: 'https://test.open-drugs.org/',
   apiMocks: {
     experimentsList: '../assets/mocks/experiments-list-mock.json',
+    speciesList: '../assets/mocks/species-list-mock.json',
     plotData: '../assets/mocks/plot-data-mock.json'
   },
   languages: ['ru', 'en', 'zh'],
