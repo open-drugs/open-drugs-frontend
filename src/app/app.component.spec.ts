8a28c132658b5292d0656c3f510199640ff70276
Constantine <kon.rafikov@yandex.ru>
2022-03-16 12:56:49 -0300

    Set up temporal Firebase hosting. Project configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'listcontrols'`, () => {
+  it(`should have as title 'open-drugs-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('listcontrols');
+    // expect().matcher
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('listcontrols app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('open-drugs-frontend app is running!');
   });
 });
